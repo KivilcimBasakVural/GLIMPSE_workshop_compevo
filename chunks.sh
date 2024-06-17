f797d98f2a91679d9bc8a1e981bc3e999e286ab3
KivilcimBasakVural <74351346+KivilcimBasakVural@users.noreply.github.com>
2024-06-17 23:57:51 +0300

    chunk.sh
    create chunk for each chr

---
+++ chunks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -p chimp
+#SBATCH -n 1
+#SBATCH -t 5-00:00:00
+#SBATCH -J refpanel
+#SBATCH -o slurm-%j-%N-%u.out
+#SBATCH -e slurm-%J-%N-%u.err
+
+chunk=/usr/local/sw/GLIMPSE/tutorial/bin/GLIMPSE2_chunk
+chr=$1
+outdir=$2
+indir=/mnt/NEOGENE3/share/dna/hsa/genotypes/1000G_20130502/reference_panel
+mapdir=/usr/local/sw/GLIMPSE/maps/genetic_maps.b37
+
+${chunk} --input ${indir}/1000GP.chr${chr}.sites.vcf.gz --region ${chr} --sequential --output ${outdir}/chunks.chr${chr}.txt --map ${mapdir}/chr${chr}.b37.gmap.gz
