e8f9853950a14d266d078b26a0643895107b27d4
KivilcimBasakVural <74351346+KivilcimBasakVural@users.noreply.github.com>
2024-06-18 00:29:48 +0300

    ligate.sh

---
+++ ligate.sh
@@ -0,0 +1,25 @@
+#SBATCH -p chimp
+#SBATCH -n 1
+#SBATCH -t 5-00:00:00
+#SBATCH -J ligate
+#SBATCH -o slurm-%j-%N-%u.out
+#SBATCH -e slurm-%J-%N-%u.err
+
+
+bcftools=/usr/local/sw/bcftools-1.18/bcftools
+tabix=/usr/local/sw/bcftools-1.18/htslib-1.18/tabix
+ligate=/usr/local/sw/GLIMPSE/tutorial/bin/GLIMPSE2_ligate
+
+bam=$1
+
+sample="$( cut -d "_" -f1 <<< "${bam}" )"; samplename=${sample##*/}; echo "$samplename"
+filebase=$(basename $bam .trimBAM.bam)
+
+for chr in {1..22}
+do
+        LST=${samplename}/${samplename}_ligate/list.chr${chr}.txt
+        ls -1v ${samplename}/${samplename}_impute/${filebase}_chr${chr}_*.*bcf >${LST}
+        OUT=${samplename}/${samplename}_ligate/${filebase}_chr${chr}.imputed.merged.bcf
+        ${ligate} --input ${LST} --output ${OUT}
+        ${bcftools} index -f ${OUT}
+done
