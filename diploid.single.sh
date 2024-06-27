550910c0c2ab8b33a69719a5d8a4665dd3f88353
KivilcimBasakVural <74351346+KivilcimBasakVural@users.noreply.github.com>
2024-06-27 16:27:21 +0300

    Create diploid.single.sh

---
+++ diploid.single.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -p gibbon
+#SBATCH -n 8
+#SBATCH -t 5-00:00:00
+#SBATCH -J diploid
+#SBATCH -o slurm-%j-%N-%u.out
+#SBATCH -e slurm-%J-%N-%u.err
+
+bam=$1 ##AKT16_merged.hs37d5.cons.90perc.down_chr
+chr=$2
+sample="$( cut -d "_" -f1 <<< "${bam}" )"; samplename=${sample##*/}; echo "$samplename"
+filebase=$(basename $bam .trimBAM.bam)
+
+bcftools=/usr/local/sw/bcftools-1.18/bcftools
+
+BAM=${samplename}/${samplename}_bam/${filebase}_chr${chr}.bam
+REFGEN=/mnt/NEOGENE3/share/ref/genomes/hsa/chr/hs37d5.chr${chr}.fa
+VCF=/mnt/NEOGENE3/share/dna/hsa/genotypes/1000G_20130502/reference_panel/1000GP.chr${chr}.sites.vcf.gz
+TSV=/mnt/NEOGENE3/share/dna/hsa/genotypes/1000G_20130502/reference_panel/1000GP.chr${chr}.sites.tsv.gz
+OUT=${samplename}/${samplename}_vcf/${filebase}_chr${chr}.vcf.gz
+
+${bcftools} mpileup -f ${REFGEN} -I -E -a 'FORMAT/DP' -T ${VCF} -r ${chr} -q 30 -Q 30 --threads 8 ${BAM} -Ou | ${bcftools} call -Aim -C alleles -T ${TSV} -Oz -o ${OUT}
+${bcftools} index -f ${OUT}
