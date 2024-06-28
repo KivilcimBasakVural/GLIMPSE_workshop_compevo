646be60c2e6e1a4b3d1ab0c272c823e2fbcf14c7
KivilcimBasakVural <74351346+KivilcimBasakVural@users.noreply.github.com>
2024-06-28 13:27:18 +0300

    Update files.sh

--- files.sh
+++ files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for bam in $(cat bamlist);do sample="$( cut -d "_" -f1 <<< "${bam}" )"; samplename=${sample##*/}; echo "$samplename";done >list
 for i in $(cat list);do mkdir ${i};done
-for i in $(cat list);do mkdir ${i}/${i}_bam;done
+##for i in $(cat list);do mkdir ${i}/${i}_bam;done
 for i in $(cat list);do mkdir ${i}/${i}_vcf;done
 for i in $(cat list);do mkdir ${i}/${i}_impute;done
 for i in $(cat list);do mkdir ${i}/${i}_ligate;done
